fa3abbd654d2fd579dc13fbcdfaf26b7531a9119
manideep-18 <r8manideep@gmail.com>
2021-04-08 11:39:13 +0530

    password validation updated

--- src/utils/validationUtils.js
+++ src/utils/validationUtils.js
@@ -1,6 +1,6 @@
 export function validatePassword(value) {
-  if (value.length < 6) return "Minimum length 6 characters";
   if (!/^\S*$/.test(value)) return "Cannot have spaces";
+  if (value.length < 6) return "Minimum length 6 characters";
   if (!/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/.test(value)) {
     return "Must have a number and alphabet";
   }
