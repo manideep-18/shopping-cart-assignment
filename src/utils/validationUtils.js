e325d8ea113ba08f3b3803a5e13b14a73c330bec
manideep-18 <r8manideep@gmail.com>
2021-05-06 18:48:24 +0530

    special characters allowed in password validation

--- src/utils/validationUtils.js
+++ src/utils/validationUtils.js
@@ -1,7 +1,11 @@
 export function validatePassword(value) {
   if (!/^\S*$/.test(value)) return "Cannot have spaces";
   if (value.length < 6) return "Minimum length 6 characters";
-  if (!/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/.test(value)) {
+  if (
+    !/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9!~@#$%^&*+=`|{}:;!.?\"()\[\]-]+)$/.test(
+      value
+    )
+  ) {
     return "Must have a number and alphabet";
   }
 }
